3c59333d1d890c336fac6dc6d2d48ebec524f222
Natakit Lalitputtichoke <choffonc@gmail.com>
2019-08-17 23:49:16 +0700

    สร้าง gui หน้าเลือกหนังที่จะจองที่นั่ง

--- src/cinema/Main.java
+++ src/cinema/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package cinema;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("mockupmovie.fxml"));
+        primaryStage.setTitle("Booking Movie");
+        primaryStage.setScene(new Scene(root, 600, 658));
         primaryStage.show();
     }
 
