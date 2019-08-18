77b1ff38505a095f088b8c09f39d2f387df03bd1
Natakit Lalitputtichoke <choffonc@gmail.com>
2019-08-18 19:29:15 +0700

    สร้าง gui ของโรงหนังโรงที่ 1 ถึง 4 สร้างmethod handlesummitonaction เป็นmethod เมื่อเรากดปุ่ม 10.00 ของหนังโรงที่1จะไปยังหน้าจองที่นั่งของหนังเรื่องที่1

--- src/cinema/Cinema.java
+++ src/cinema/Cinema.java
@@ -1,4 +1,23 @@
 package cinema;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Cinema {
+    @FXML
+    Button booktheatre1 ;
+
+    @FXML public void handleSubmitBtnOnAction(ActionEvent event)throws IOException {
+        Button b = (Button) event.getSource() ;
+        Stage s = (Stage) b.getScene().getWindow() ;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("chair1.fxml"));
+        s.setScene(new Scene(loader.load(),600,550));
+        s.show();
+    }
 }
