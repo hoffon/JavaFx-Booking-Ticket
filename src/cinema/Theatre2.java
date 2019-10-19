cc5f4a60d4d9594b2cc22a96a13df4dcd79a4c06
Natakit Lalitputtichoke <choffonc@gmail.com>
2019-10-20 00:35:18 +0700

    -เพิ่มระบบจองของโรงที่ 2

--- src/cinema/Theatre2.java
+++ src/cinema/Theatre2.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.image.Image;
@@ -23,40 +24,55 @@ public class Theatre2 implements backToFirstPage{
 
     private int sumPrice = 0  ;
 
-    ArrayList<Chair4K> chairs  ;
+    ArrayList<NormalChair> chairs  ;
     ArrayList<String> chairsSelected ;
-    Chair4K chairC0,chairC1,chairC2,chairC3,chairC4,chairC5,chairC6,
+    NormalChair chairD0,chairD1,chairD2,chairD3,chairD4,chairD5,chairD6,
+            chairC0,chairC1,chairC2,chairC3,chairC4,chairC5,chairC6,
             chairB0,chairB1,chairB2,chairB3,chairB4,chairB5,chairB6,
             chairA0,chairA1,chairA2,chairA3,chairA4,chairA5,chairA6;
 
-    private MyFile fileBookingTheatre4  ;
+    private MyFile fileBookingTheatre2  ;
 
     @FXML  public void initialize() {
         String fs = File.separator;
-        fileBookingTheatre4 = new MyFile(System.getProperty("user.dir") + fs + "bookingchair" + fs + "chairselectedtheatre4", "chairbookingtheatre4.txt");
-        chairC0 = new Chair4K(c0, chairc0);
-        chairC1 = new Chair4K(c1, chairc1);
-        chairC2 = new Chair4K(c2, chairc2);
-        chairC3 = new Chair4K(c3, chairc3);
-        chairC4 = new Chair4K(c4, chairc4);
-        chairC5 = new Chair4K(c5, chairc5);
-        chairC6 = new Chair4K(c6, chairc6);
-        chairB0 = new Chair4K(b0, chairb0);
-        chairB1 = new Chair4K(b1, chairb1);
-        chairB2 = new Chair4K(b2, chairb2);
-        chairB3 = new Chair4K(b3, chairb3);
-        chairB4 = new Chair4K(b4, chairb4);
-        chairB5 = new Chair4K(b5, chairb5);
-        chairB6 = new Chair4K(b6, chairb6);
-        chairA0 = new Chair4K(a0, chaira0, 400);
-        chairA1 = new Chair4K(a1, chaira1, 400);
-        chairA2 = new Chair4K(a2, chaira2, 400);
-        chairA3 = new Chair4K(a3, chaira3, 400);
-        chairA4 = new Chair4K(a4, chaira4, 400);
-        chairA5 = new Chair4K(a5, chaira5, 400);
-        chairA6 = new Chair4K(a6, chaira6, 400);
+        fileBookingTheatre2 = new MyFile(System.getProperty("user.dir") + fs + "bookingchair" + fs + "chairselectedtheatre2", "chairbookingtheatre2.txt");
+        chairD0 = new NormalChair(d0,chaird0);
+        chairD1 = new NormalChair(d1,chaird1);
+        chairD2 = new NormalChair(d2,chaird2);
+        chairD3 = new NormalChair(d3,chaird3);
+        chairD4 = new NormalChair(d4,chaird4);
+        chairD5 = new NormalChair(d5,chaird5);
+        chairD6 = new NormalChair(d6,chaird6);
+        chairC0 = new NormalChair(c0,chairc0);
+        chairC1 = new NormalChair(c1,chairc1);
+        chairC2 = new NormalChair(c2,chairc2);
+        chairC3 = new NormalChair(c3,chairc3);
+        chairC4 = new NormalChair(c4,chairc4);
+        chairC5 = new NormalChair(c5,chairc5);
+        chairC6 = new NormalChair(c6,chairc6);
+        chairB0 = new NormalChair(b0,chairb0);
+        chairB1 = new NormalChair(b1,chairb1);
+        chairB2 = new NormalChair(b2,chairb2);
+        chairB3 = new NormalChair(b3,chairb3);
+        chairB4 = new NormalChair(b4,chairb4);
+        chairB5 = new NormalChair(b5,chairb5);
+        chairB6 = new NormalChair(b6,chairb6);
+        chairA0 = new NormalChair(a0,chaira0,200);
+        chairA1 = new NormalChair(a1,chaira1,200);
+        chairA2 = new NormalChair(a2,chaira2,200);
+        chairA3 = new NormalChair(a3,chaira3,200);
+        chairA4 = new NormalChair(a4,chaira4,200);
+        chairA5 = new NormalChair(a5,chaira5,200);
+        chairA6 = new NormalChair(a6,chaira6,200);
         chairs = new ArrayList<>();
         chairsSelected = new ArrayList<>();
+        chairs.add(chairD0);
+        chairs.add(chairD1);
+        chairs.add(chairD2);
+        chairs.add(chairD3);
+        chairs.add(chairD4);
+        chairs.add(chairD5);
+        chairs.add(chairD6);
         chairs.add(chairC0);
         chairs.add(chairC1);
         chairs.add(chairC2);
@@ -78,10 +94,71 @@ public class Theatre2 implements backToFirstPage{
         chairs.add(chairA4);
         chairs.add(chairA5);
         chairs.add(chairA6);
+        readFile() ;
+    }
+
+    @Override
+    public void backToFirstPage(ActionEvent event) throws IOException {
+        Button b = (Button) event.getSource() ;
+        Stage s = (Stage) b.getScene().getWindow() ;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("mockupmovie.fxml"));
+        s.setScene(new Scene(loader.load(),600,658));
+        s.show();
+    }
+    public void bookingChair(ActionEvent event) throws IOException {
+        for (NormalChair chair : chairs){
+            if (chair.getBox().isSelected()) {
+                chairSelectedPrice(chair);
+            }
+        }
+        Alert td = new Alert(Alert.AlertType.CONFIRMATION);
+        td.setTitle("ยืนยันการจอง");
+        td.setContentText("ยอดที่ต้องจ่าย " +getSumPrice());
+        td.showAndWait();
+        if (td.getResult().getText().equals("OK")) {
+            for (Chair chair : chairs) {
+                if (chair.getBox().isSelected()) {
+                    chair.getBox().setDisable(true);
+                    chair.setStatusBooking(true);
+                    chair.getImage().setImage(new Image("image/chairselected.png"));
+                    String text = String.valueOf(chair.getBox());
+                    fileBookingTheatre2.appendWithNewLine(text);
+                    fileBookingTheatre2.save();
+                }
+            }
+            Button b = (Button) event.getSource() ;
+            Stage s = (Stage) b.getScene().getWindow() ;
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("mockupmovie.fxml"));
+            s.setScene(new Scene(loader.load(),600,600));
+            s.show();
+        }
+        else if (td.getResult().getText().equals("Cancel")) setSumPrice(0);
+    }
+    public void mouseClick(){
+        for (Chair chair : chairs){
+            if (chairsSelected.contains(String.valueOf(chair.getBox())) )
+                chair.getImage().setImage(new Image("image/chairselected.png"));
+            else if (chair.getBox().isSelected()){
+                chair.getImage().setImage(new Image("image/chairselect.png"));
+            }
+            else chair.getImage().setImage(new Image("image/chair.png"));
+        }
+    }
+    public int getSumPrice() {
+        return sumPrice;
+    }
+    public void chairSelectedPrice(NormalChair price){
+        sumPrice += price.getNormalChairPrice();
+    }
+    public void setSumPrice(int sumPrice) {
+        this.sumPrice = sumPrice;
+    }
+
+    public void readFile(){
         try {
-            File file = new File(fileBookingTheatre4.getDirectory());
+            File file = new File(fileBookingTheatre2.getDirectory());
             if (!file.exists()) file.mkdirs();
-            FileReader fileReader = new FileReader(fileBookingTheatre4.getFilename());
+            FileReader fileReader = new FileReader(fileBookingTheatre2.getFilename());
             BufferedReader buffer = new BufferedReader(fileReader);
             String line ;
             while ((line = buffer.readLine()) != null) {
@@ -95,28 +172,11 @@ public class Theatre2 implements backToFirstPage{
             }
         }
         catch (FileNotFoundException e){
-            System.err.println("Cannot read file "+fileBookingTheatre4.getFilename());
+            System.err.println("Cannot read file "+fileBookingTheatre2.getFilename());
         }
         catch (IOException e){
             System.err.println("Error reading from file");
         }
-    }
 
-    @Override
-    public void backToFirstPage(ActionEvent event) throws IOException {
-        Button b = (Button) event.getSource() ;
-        Stage s = (Stage) b.getScene().getWindow() ;
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("mockupmovie.fxml"));
-        s.setScene(new Scene(loader.load(),600,658));
-        s.show();
-    }
-    public int getSumPrice() {
-        return sumPrice;
-    }
-    public void chairSelectedPrice(NormalChair price){
-        sumPrice += price.getNormalChairPrice();
-    }
-    public void setSumPrice(int sumPrice) {
-        this.sumPrice = sumPrice;
     }
 }
