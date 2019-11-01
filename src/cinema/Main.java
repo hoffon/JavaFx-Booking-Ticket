576e216b118db1ac38693df05531e67f99a0b29a
Natakit Lalitputtichoke <choffonc@gmail.com>
2019-11-01 23:30:42 +0700

    -update readme

--- src/cinema/Main.java
+++ src/cinema/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
-        primaryStage.setTitle("Booking Movie");
+        primaryStage.setTitle("KU BOOKING TICKET");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
